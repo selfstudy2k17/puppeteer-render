7466bbc9307303b5af1a569bd535876f3150aaeb
selfstudy2k17 <139640219+selfstudy2k17@users.noreply.github.com>
2023-12-26 15:08:31 +0530

    Update scrapeLogic.js
    I just changed the headless to be false.

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 const scrapeLogic = async (res,url) => {
-  const browser = await puppeteer.launch({ headless: true },{
+  const browser = await puppeteer.launch({ headless: false },{
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
